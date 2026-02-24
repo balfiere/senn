0cd61a57c19b34a9657947c499df957e74e78084
balfiere <balfiere@proton.me>
2026-02-23 20:45:07 -0600

    update some default environmental settings

--- config/pdf.php
+++ config/pdf.php
@@ -2,8 +2,9 @@
 
 return [
     'cache_strategy' => env('PDF_CACHE_STRATEGY', 'local'), // local | cdn
-    'cache_ttl' => (int) env('PDF_CACHE_TTL', 86400),
+    'cache_ttl' => (int) env('PDF_CACHE_TTL', 2592000),
 
+    // not yet implemented
     // used only for `cdn`
-    'cdn_base_url' => env('PDF_CDN_BASE_URL'), // e.g. https://cdn.example.com
+    // 'cdn_base_url' => env('PDF_CDN_BASE_URL'), // e.g. https://cdn.example.com
 ];
--- config/session.php
+++ config/session.php
@@ -185,7 +185,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
